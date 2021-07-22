f71f34e79724353b706443f1b522cc00fb7e5b6c
jtanyag <jtanyag@gmail.com>
2021-07-22 10:06:47 -0700

    creates HeroImage component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 // Config
 import { BACKDROP_SIZE, IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // Components
-
+import HeroImage from './HeroImage';
 // Hooks
 import { useHomeFetch } from '../hooks/useHomeFetch';
 // Images
@@ -12,10 +12,16 @@ const Home = () => {
   const { state, loading, error } = useHomeFetch();
 
   return (
-    <div>
-      Home Page
-    </div>
-  )
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
