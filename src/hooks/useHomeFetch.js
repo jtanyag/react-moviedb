2c833b82f80b6dcff23da1a397729f9a0fbfa9cb
jtanyag <jtanyag@gmail.com>
2021-07-23 08:39:53 -0700

    creates SearchBar component and logic

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -9,6 +9,7 @@ const initialState = {
 }
 
 export const useHomeFetch = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -29,9 +30,11 @@ export const useHomeFetch = () => {
     setLoading(false);
   };
 
+  // Initial render and search
   useEffect(() => {
-    fetchMovies(1);
-  }, []);
+    setState(initialState);
+    fetchMovies(1, searchTerm);
+  }, [searchTerm]);
 
-  return { state, loading, error };
+  return { state, loading, error, searchTerm, setSearchTerm };
 };
