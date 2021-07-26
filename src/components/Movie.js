246bb66ac5e1e6710111508bb6aa3d21e533d352
jtanyag <jtanyag@gmail.com>
2021-07-26 10:46:55 -0700

    creates breadcrumb for movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,6 +5,7 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // Components
 import Grid from './Grid';
 import Spinner from './Spinner';
+import BreadCrumb from './BreadCrumb';
 // Hooks
 import { useMovieFetch } from '../hooks/useMovieFetch';
 // Image
@@ -14,11 +15,12 @@ const Movie = () => {
   const { movieId } = useParams();
   const { state: movie, loading, error }= useMovieFetch(movieId);
 
-  console.log(movie);
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
 
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
     </>
   );
 };
