6aed19df9a6634c4eda7acdf1851b8cd23834832
jtanyag <jtanyag@gmail.com>
2021-07-26 08:37:53 -0700

    creates routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Components
 import Header from './components/Header';
-import Home from'./components/Home';
+import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 // Styles
 import { GlobalStyle } from './GlobalStyle';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <GlobalStyle />
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/:movieId' element={<Movie />} />
+      <Route path='/*' element={<NotFound />} />
+    </Routes>
+    <GlobalStyle />
+  </BrowserRouter>
+);
+
 
 export default App;
